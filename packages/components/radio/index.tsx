c9a5f8b3d93aff90ff4cd7f38068580e4158b476
iwant <53087594+1want@users.noreply.github.com>
2022-09-15 00:06:28 +0800

    ts6

--- packages/components/radio/index.tsx
+++ packages/components/radio/index.tsx
@@ -2,6 +2,7 @@
 import RadioGroup from './radio-group'
 import './index.css'
 
-// const Radio = Object.assign(_Radio, { Group: RadioGroup })
-const Radio = {}
+const _Radio = {}
+
+const Radio = Object.assign(_Radio, { Group: RadioGroup })
 export { Radio }
