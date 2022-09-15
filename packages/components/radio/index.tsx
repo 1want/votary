74d1df327992795f1d2f8736ed31cc41bc557889
iwant <53087594+1want@users.noreply.github.com>
2022-09-15 22:22:00 +0800

    ts2

--- packages/components/radio/index.tsx
+++ packages/components/radio/index.tsx
@@ -1,7 +1,41 @@
-import Radio from './radio'
+import { useContext } from 'react'
+import classNames from 'classnames'
+
+import RadioContext from './radio-context'
 import RadioGroup from './radio-group'
 
-import './index.css'
+interface RadioProps {
+  defaultValue?: string
+  checked?: boolean
+  disabled?: boolean
+  children?: any
+  value?: string
+}
 
-// const Radio = Object.assign(_Radio, { Group: RadioGroup })
+const Radio = (props: RadioProps) => {
+  const { ...context } = useContext(RadioContext)
+  const classes = classNames([props.disabled && 'is-disabled'])
+  const checked = context.checked
+    ? context.checked === props.value
+    : props.checked || false
+
+  const onChange = () => {
+    context.toggle?.(props.value || '')
+  }
+
+  return (
+    <div className='radio-item'>
+      <input
+        className={classes}
+        type='radio'
+        checked={checked}
+        onChange={onChange}
+      />
+      <span className='radio-name'>{props.children}</span>
+    </div>
+  )
+}
+
+Radio.Group = RadioGroup
+
 export { Radio }
