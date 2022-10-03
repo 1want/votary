59153caec2d836a906fc52c873b3537938124540
iwant <53087594+1want@users.noreply.github.com>
2022-10-03 12:44:50 +0800

    完善menu菜单动画

--- docs/components/menu/basic.tsx
+++ docs/components/menu/basic.tsx
@@ -3,12 +3,12 @@ import { Menu } from '../../../packages/components/menu'
 export default () => {
   return (
     <Menu>
-      <Menu.SubMenu title='通用配置' index='1' icon='house'>
-        <Menu.Item>1</Menu.Item>
-        <Menu.Item>2</Menu.Item>
+      <Menu.SubMenu title='一年级一班' index='1' icon='house'>
+        <Menu.Item>101</Menu.Item>
+        <Menu.Item>102</Menu.Item>
       </Menu.SubMenu>
-      <Menu.SubMenu title='系统设置' index='2' icon='message'>
-        <Menu.Item>3</Menu.Item>
+      <Menu.SubMenu title='一年级二班' index='2' icon='message'>
+        <Menu.Item>201</Menu.Item>
       </Menu.SubMenu>
     </Menu>
   )
--- packages/components/menu/sub-menu.tsx
+++ packages/components/menu/sub-menu.tsx
@@ -1,26 +1,24 @@
 import { useState } from 'react'
-import { useSpring } from 'react-spring'
+import { useSpring, animated } from 'react-spring'
 import useMeasure from 'react-use-measure'
-import { CSSTransition } from 'react-transition-group'
 import { Icon } from '../../icon'
-
 import { SubMenuProps } from './types'
 
 const Menu = (props: SubMenuProps) => {
   const { children, title, icon } = props
-  const [visible, setVisible] = useState(false)
 
   const [ref, { height: viewHeight }] = useMeasure()
-  const { height } = useSpring({
+
+  const [open, setOpen] = useState(false)
+  const propss = useSpring({
     from: { height: 0 },
     to: {
-      height: visible ? viewHeight : 0
+      height: open ? viewHeight : 0
     }
   })
-
   return (
     <div className='sub-menu'>
-      <div className='sub-menu-header' onClick={() => setVisible(!visible)}>
+      <div className='sub-menu-header' onClick={() => setOpen(prev => !prev)}>
         <div className='left'>
           <Icon name={icon}></Icon>
           <span className='sub-menu-title'>{title}</span>
@@ -28,13 +26,15 @@ const Menu = (props: SubMenuProps) => {
         <span className='iconfont icon-arrow-down'></span>
       </div>
 
-      <ul className='sub-content' style={{ animation: height }}>
-        {children}
-      </ul>
-
-      {/* <CSSTransition classNames='menu' unmountOnExit timeout={400} in={visible}>
-        <ul className='sub-content'>{children}</ul>
-      </CSSTransition> */}
+      <animated.div
+        style={{
+          ...propss,
+          overflow: 'hidden'
+        }}>
+        <ul ref={ref} className='sub-content'>
+          {children}
+        </ul>
+      </animated.div>
     </div>
   )
 }
